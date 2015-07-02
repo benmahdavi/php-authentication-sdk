99336bfe9329dfc1aa9fadf99541e9fcffb6cfb4
Paul Higgins <paulhigginz@gmail.com>
2015-07-02 14:04:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # php-authentication-sdk
-Simple PHP SDK to generate a token that can be used to authenticate with the Crowd Valley API
+PHP SDK for the Crowd Valley API 
+
+## Usage
+
+Open ‘generate-header.php’ and enter your values for:
+
+- Username
+- Password
+- Network Name
+- API Key
+- API Secret
+
+Then, in the Terminal or Command Prompt, go to the ‘php-sdk’ folder run the command ‘php generate-header.php’. This will print out a token in the form of a text string that you can enter as the ‘cv-auth’ header when making calls to the Crowd Valley API. It will look something like this:
+
+AuthToken ApiKey=“your—api-key-0001", TokenDigest="cFziWZJsfSl5T3R8hfZDUkBWZ68=", Nonce="8d09e353dfa721547d3fa9fe64770029", Created="2015-07-01T13:51:24+01:00", Username=“yourname@youremail.com”, Password="Xl1qQGL1KHtIVzkyGylHWQ=="
+
+Make sure that you copy the full string including the double quote mark at the end.
+
+If the command does not print out a token in this format or if it gives any other error then please check that you have PHP installed on your machine. 
+
+## Support
+
+Please contact Crowd Valley at support@crowdvalley.com for any questions.
---
+++ generate-header.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Generate a token to authenticate with the Crowd Valley API
+*/
+
+include_once('Header.php');
+include_once('Encrypt.php');
+
+// Enter your Crowd Valley API Key and Secret
+$apiKey = 'your-api-key';
+$apiSecret = 'your-api-secret';
+
+// Enter your Username and Password
+$username = 'yourname@youremail.com';
+$password = 'yourpassword';
+
+// Enter your Network Name
+$network = 'yournetworkname';
+
+// Enter the API endpoint. This should be "https://sandbox.crowdvalley.com/v1" 
+// unless you have a paid account with Crowd Valley and you are using the live API
+$apiURL = "https://sandbox.crowdvalley.com/v1";
+
+//
+//
+// No need to touch anything below this line
+//
+//
+
+// if API is under Basic Authentication, enter BasicAuth username and password
+$apiBasicUsername = '';
+$apiBasicPassword = '';
+
+$header = new Header($apiKey, $apiSecret);
+$encrypt = new Encrypt();
+$encryptedPass = $encrypt->encrypt($apiSecret,$password,16);
+
+$token = $header->createToken($username, $encryptedPass);
+
+echo $token."\n\n";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERPWD, $apiBasicUsername . ":" . $apiBasicPassword );
+
+$headers = array();
+$headers[] = 'cv-auth: '.$token;
+$headers[] = 'network: '.$network;
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+print  $server_output."\n";
